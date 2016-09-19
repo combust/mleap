f5b850b65fa59057516551725a11a39a2a8be786
Mikhail Semeniuk <seme0021@gmail.com>
2016-09-18 22:19:15 -0700

    adding MaxAbsScaler

---
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/MaxAbsScaler.scala
@@ -0,0 +1,27 @@
+package ml.combust.mleap.runtime.transformer.feature
+
+import ml.bundle.dsl.Model
+import ml.combust.mleap.core.feature.MaxAbsScalerModel
+import ml.combust.mleap.runtime.transformer.Transformer
+import ml.combust.mleap.runtime.transformer.builder.TransformBuilder
+import ml.combust.mleap.runtime.types.TensorType
+
+import scala.util.Try
+
+/**
+  * Created by mikhail on 9/18/16.
+  */
+case class MaxAbsScaler(
+                       uid: String = Transformer.uniqueName("max_abs_scaler"),
+                       inputCol: String,
+                       outputCol: String,
+                       model: MaxAbsScalerModel
+                       ) extends Transformer{
+
+  override def transform[TB <: TransformBuilder[TB]](builder: TB): Try[TB] = {
+    builder.withInput(inputCol, TensorType.doubleVector()).flatMap {
+      case (b, inputIndex) =>
+        b.withOutput(outputCol, TensorType.doubleVector())(row => model(row.getVector(inputIndex)))
+    }
+  }
+}

05d2737a7c11f64deb3ee249e70a9a770e309544
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-09-21 02:17:01 -0400

    Output probabilities for ProbabilisticClassificationModel.

--- mleap-spark/src/main/scala/org/apache/spark/ml/mleap/classification/OneVsRest.scala
+++ mleap-spark/src/main/scala/org/apache/spark/ml/mleap/classification/OneVsRest.scala
@@ -30,7 +30,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.annotation.Since
 import org.apache.spark.ml._
 import org.apache.spark.ml.attribute._
-import org.apache.spark.ml.classification.{ClassificationModel, Classifier}
+import org.apache.spark.ml.classification.{ClassificationModel, Classifier, ProbabilisticClassificationModel, ProbabilisticClassifier}
 import org.apache.spark.ml.linalg.Vector
 import org.apache.spark.ml.param.shared.HasProbabilityCol
 import org.apache.spark.ml.param.{Param, ParamMap, ParamPair, Params}
@@ -171,7 +171,10 @@ final class OneVsRestModel private[ml] (
     // update the accumulator column with the result of prediction of models
     val aggregatedDataset = models.zipWithIndex.foldLeft[DataFrame](newDataset) {
       case (df, (model, index)) =>
-        val rawPredictionCol = model.getRawPredictionCol
+        val rawPredictionCol = model match {
+          case model: ProbabilisticClassificationModel[_, _] => model.getProbabilityCol
+          case _ => model.getRawPredictionCol
+        }
         val columns = origCols ++ List(col(rawPredictionCol), col(accColName))
 
         // add temporary column to store intermediate scores and update

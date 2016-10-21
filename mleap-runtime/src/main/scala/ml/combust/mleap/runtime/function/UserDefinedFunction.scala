3499d00f8f8deccbeb1b1dc6b24b9933735e5f34
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-10-21 15:25:57 -0700

    Finish refactoring and testing user defined functions.

--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/function/UserDefinedFunction.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/function/UserDefinedFunction.scala
@@ -1,19 +1,15 @@
 package ml.combust.mleap.runtime.function
 
 import ml.combust.mleap.runtime.types._
-import org.apache.spark.ml.linalg.Vector
 
 import scala.language.implicitConversions
+import scala.reflect.runtime.universe.TypeTag
 
 /**
   * Created by hollinwilkins on 10/19/16.
   */
 object UserDefinedFunction {
-  val universe: scala.reflect.runtime.universe.type = scala.reflect.runtime.universe
-  def mirror: universe.Mirror = synchronized {
-    universe.runtimeMirror(Thread.currentThread().getContextClassLoader)
-  }
-  import universe._
+  import ml.combust.mleap.runtime.reflection.MleapReflection.dataType
 
   implicit def apply[RT: TypeTag](f: () => RT): UserDefinedFunction =
     UserDefinedFunction(f, dataType[RT], Seq())
@@ -27,26 +23,6 @@ object UserDefinedFunction {
     UserDefinedFunction(f, dataType[RT], Seq(dataType[T1], dataType[T2], dataType[T3], dataType[T4]))
   implicit def apply[RT: TypeTag, T1: TypeTag, T2: TypeTag, T3: TypeTag, T4: TypeTag, T5: TypeTag](f: (T1, T2, T3, T4, T5) => RT): UserDefinedFunction =
     UserDefinedFunction(f, dataType[RT], Seq(dataType[T1], dataType[T2], dataType[T3], dataType[T4], dataType[T5]))
-
-  private def dataType[T: TypeTag]: DataType = dataTypeFor(mirrorType[T])
-  private def dataTypeFor(tpe: `Type`): DataType = synchronized {
-    tpe match {
-      case t if t <:< mirrorType[Boolean] => BooleanType
-      case t if t <:< mirrorType[String] => StringType
-      case t if t <:< mirrorType[Int] => IntegerType
-      case t if t <:< mirrorType[Long] => LongType
-      case t if t <:< mirrorType[Double] => DoubleType
-      case t if t <:< mirrorType[Array[_]] =>
-        val TypeRef(_, _, Seq(elementType)) = t
-        val baseType = dataTypeFor(elementType)
-        ListType(baseType)
-      case t if t <:< mirrorType[Vector] => TensorType.doubleVector()
-      case t if t <:< mirrorType[Any] => AnyType
-      case t => throw new IllegalArgumentException(s"unknown type $t")
-    }
-  }
-
-  private def mirrorType[T: TypeTag]: `Type` = typeTag[T].in(mirror).tpe.normalize
 }
 
 case class UserDefinedFunction(f: AnyRef,
---
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/reflection/MleapReflection.scala
@@ -0,0 +1,43 @@
+package ml.combust.mleap.runtime.reflection
+
+import ml.combust.mleap.runtime.types._
+import org.apache.spark.ml.linalg.Vector
+
+/**
+  * Created by hollinwilkins on 10/21/16.
+  */
+object MleapReflectionLock
+
+trait MleapReflection {
+  val universe: scala.reflect.runtime.universe.type
+  def mirror: universe.Mirror
+
+  import universe._
+
+  def dataType[T: TypeTag]: DataType = dataTypeFor(mirrorType[T])
+  private def dataTypeFor(tpe: `Type`): DataType = MleapReflectionLock.synchronized {
+    tpe match {
+      case t if t <:< mirrorType[Boolean] => BooleanType
+      case t if t <:< mirrorType[String] => StringType
+      case t if t <:< mirrorType[Int] => IntegerType
+      case t if t <:< mirrorType[Long] => LongType
+      case t if t <:< mirrorType[Double] => DoubleType
+      case t if t <:< mirrorType[Array[_]] =>
+        val TypeRef(_, _, Seq(elementType)) = t
+        val baseType = dataTypeFor(elementType)
+        ListType(baseType)
+      case t if t <:< mirrorType[Vector] => TensorType.doubleVector()
+      case t if t =:= mirrorType[Any] => AnyType
+      case t => throw new IllegalArgumentException(s"unknown type $t")
+    }
+  }
+
+  private def mirrorType[T: TypeTag]: `Type` = typeTag[T].in(mirror).tpe.normalize
+}
+
+object MleapReflection extends MleapReflection {
+  override val universe: scala.reflect.runtime.universe.type = scala.reflect.runtime.universe
+  override def mirror: universe.Mirror = MleapReflectionLock.synchronized {
+    universe.runtimeMirror(Thread.currentThread().getContextClassLoader)
+  }
+}

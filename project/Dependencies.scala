59999d9fac45b568ee7b0d5b11544362dcea9ff1
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-10-24 15:16:34 +0200

    Mark spark-mllib-local as a provided dependency.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
 
   lazy val baseDependencies = Seq("org.scalatest" %% "scalatest" % "3.0.0" % "test")
 
-  lazy val sparkLocalDependencies = Seq("org.apache.spark" %% "spark-mllib-local" % sparkVersion)
+  lazy val sparkLocalDependencies = Seq("org.apache.spark" %% "spark-mllib-local" % sparkVersion % "provided")
   lazy val sparkDependencies = Seq("org.apache.spark" %% "spark-core" % sparkVersion,
     "org.apache.spark" %% "spark-sql" % sparkVersion,
     "org.apache.spark" %% "spark-mllib" % sparkVersion,
@@ -19,8 +19,8 @@ object Dependencies {
       union(Seq("org.scala-lang" % "scala-reflect" % scalaVersion))
   }
 
-  lazy val mleapSparkDependencies = baseDependencies
-    .union(sparkDependencies)
+  lazy val mleapSparkDependencies = baseDependencies.
+    union(sparkDependencies)
 
   lazy val bundleMlDependencies = baseDependencies.
     union(Seq("io.spray" %% "spray-json" % "1.3.2",

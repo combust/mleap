7867124b05af9d88fe65e65dad95b6d105d2eaa8
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-10-26 08:07:22 +0200

    Get rid of reverse reflection.

--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/reflection/MleapReflection.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/reflection/MleapReflection.scala
@@ -14,28 +14,6 @@ trait MleapReflection {
 
   import universe._
 
-  def scalaType(t: DataType): `Type` = t match {
-    case BooleanType => mirrorType[Boolean]
-    case StringType => mirrorType[String]
-    case IntegerType => mirrorType[Int]
-    case LongType => mirrorType[Long]
-    case DoubleType => mirrorType[Double]
-    case ListType(base) => base match { // TODO: support arbitrary depth arrays
-      case BooleanType => mirrorType[Array[Boolean]]
-      case StringType => mirrorType[Array[String]]
-      case IntegerType => mirrorType[Array[Int]]
-      case LongType => mirrorType[Array[Long]]
-      case DoubleType => mirrorType[Array[Double]]
-      case AnyType => mirrorType[Array[Any]]
-      case tt: TensorType if tt.dimensions.length == 1 => mirrorType[Array[Vector]]
-      case _: ListType => throw new IllegalArgumentException("deep arrays not supported yet")
-      case _ => throw new IllegalArgumentException(s"unknown base type: $base")
-    }
-    case tt: TensorType if tt.dimensions.length == 1 => mirrorType[Vector]
-    case AnyType => mirrorType[Any]
-    case _ => throw new IllegalArgumentException(s"unknown type: $t")
-  }
-
   def dataType[T: TypeTag]: DataType = dataTypeFor(mirrorType[T])
   private def dataTypeFor(tpe: `Type`): DataType = MleapReflectionLock.synchronized {
     tpe match {

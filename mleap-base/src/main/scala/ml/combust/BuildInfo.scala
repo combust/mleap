6695263b0a0dfbeed6aea5a4a695fddbb61aa9ff
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-11-02 22:18:32 -0700

    Generate build info in MLeap base.

---
+++ mleap-base/src/main/scala/ml/combust/BuildInfo.scala
@@ -0,0 +1,8 @@
+package ml.combust
+
+/**
+  * Created by hollinwilkins on 11/2/16.
+  */
+case class BuildInfo(name: String = BuildValues.name,
+                     version: String = BuildValues.version,
+                     gitSha: String = BuildValues.gitHeadCommit.get)

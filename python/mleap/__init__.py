46e5b3dbb848c3e30b586c8607807aa3df81fafa
Mikhail Semeniuk <seme0021@gmail.com>
2016-11-19 17:26:52 -0800

    initial commit of init.py

--- python/mleap/__init__.py
+++ python/mleap/__init__.py
@@ -1,7 +1,10 @@
 import mleap.feature
+import mleap.sklearn
 import pyspark.ml
+import sklearn
 import sys
 
+sys.modules['sklearn'].mleap = mleap.sklearn
 sys.modules['pyspark.ml'].mleap = mleap
 sys.modules['pyspark.ml.mleap'] = mleap
 sys.modules['pyspark.ml.mleap.feature'] = sys.modules['mleap.feature']

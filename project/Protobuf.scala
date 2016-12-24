028081cf53b554962e5ee1ceaffe829a11be89de
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-12-24 12:26:48 -0800

    Use same pattern as Akka for build files. (#51)

---
+++ project/Protobuf.scala
@@ -0,0 +1,11 @@
+package ml.combust.mleap
+
+import sbt.Keys._
+import sbt._
+import sbtprotoc.ProtocPlugin.autoImport.PB
+
+object Protobuf {
+  lazy val bundleSettings = Seq(PB.targets in Compile := Seq(scalapb.gen() -> (sourceManaged in Compile).value),
+    PB.includePaths in Compile := Seq(file("bundle-protobuf")),
+    PB.protoSources in Compile := Seq(file("bundle-protobuf")))
+}

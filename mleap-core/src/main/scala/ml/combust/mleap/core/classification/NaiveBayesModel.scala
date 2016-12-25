1068c97b8239bea942924080b8153f4c863394b3
Faizan Shabbir <fshabbir@truecar.com>
2016-12-25 12:53:25 -0800

    Break out bernoulli calculation and multinomial calculation

--- mleap-core/src/main/scala/ml/combust/mleap/core/classification/NaiveBayesModel.scala
+++ mleap-core/src/main/scala/ml/combust/mleap/core/classification/NaiveBayesModel.scala
@@ -1,7 +1,9 @@
 package ml.combust.mleap.core.classification
 
-import org.apache.spark.ml.linalg.mleap.BLAS
-import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector, Vectors, DenseMatrix}
+import ml.combust.mleap.core.annotation.SparkCode
+import ml.combust.mleap.core.classification.NaiveBayesModel.{Multinomial, Bernoulli}
+import org.apache.spark.ml.linalg.mleap.{Matrices, BLAS}
+import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector, Matrix}
 
 
 /**
@@ -11,17 +13,12 @@ import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector, Vectors, D
 
 /** Companion object for constructing NaiveBayesModel.
   */
-
+@SparkCode(uri = "https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/ml/classification/NaiveBayes.scala")
 object NaiveBayesModel {
-  def apply(numFeatures: Int,
-            numClasses: Int,
-            pi: Vector,
-            theta: DenseMatrix): NaiveBayesModel = {
-    NaiveBayesModel(numFeatures,
-      numClasses,
-      pi,
-      theta)
-  }
+
+  sealed trait ModelType
+    case object Bernoulli extends ModelType
+    case object Multinomial extends ModelType
 }
 
 /**
@@ -31,16 +28,44 @@ object NaiveBayesModel {
   * @param pi log of class priors
   * @param theta log of class conditional probabilities
   */
+@SparkCode(uri = "https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/ml/classification/NaiveBayes.scala")
 case class NaiveBayesModel (numFeatures: Int,
-                            numClasses:Int,
+                            numClasses: Int,
                             pi: Vector,
-                            theta: DenseMatrix)
+                            theta: Matrix,
+                            modelType: NaiveBayesModel.ModelType)
   extends MultinomialClassificationModel with Serializable {
 
-  override def predictRaw(raw: Vector): Vector = {
+  private def multinomialCalculation(raw: Vector) = {
     val prob = theta.multiply(raw)
     BLAS.axpy(1.0, pi, prob)
     prob
+  }
+
+  private def bernoulliCalculation(raw: Vector) = {
+    val negTheta = Matrices.map(theta, value => math.log(1.0 - math.exp(value)))
+    val ones = new DenseVector(Array.fill(theta.numCols) {1.0})
+    val thetaMinusNegTheta = Matrices.map(theta, value =>
+      value - math.log(1.0 - math.exp(value)))
+    val negThetaSum = negTheta.multiply(ones);
+
+    raw.foreachActive((_, value) =>
+      require(value == 0.0 || value == 1.0,
+        s"Bernoulli naive Bayes requires 0 or 1 feature values but found $raw.")
+    )
+    val prob = thetaMinusNegTheta.multiply(raw)
+    BLAS.axpy(1.0, pi, prob)
+    BLAS.axpy(1.0, negThetaSum, prob)
+    prob
+  }
+
+  override def predictRaw(raw: Vector): Vector = {
+    modelType match {
+      case Multinomial =>
+        multinomialCalculation(raw)
+      case Bernoulli =>
+        bernoulliCalculation(raw)
+    }
   }
 
   override def rawToProbabilityInPlace(raw: Vector): Vector = {

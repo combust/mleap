5d1f182b05e63818520001c8e6e909bb18f2c7b0
Hollin Wilkins <hollinrwilkins@gmail.com>
2016-12-27 22:09:07 -0800

    Add in support for IsotonicRegression. (#75)

---
+++ mleap-core/src/main/scala/ml/combust/mleap/core/regression/IsotonicRegressionModel.scala
@@ -0,0 +1,43 @@
+package ml.combust.mleap.core.regression
+
+import java.util.Arrays.binarySearch
+
+import ml.combust.mleap.core.annotation.SparkCode
+import org.apache.spark.ml.linalg.Vector
+
+/**
+  * Created by hollinwilkins on 12/27/16.
+  */
+@SparkCode(uri = "https://github.com/apache/spark/blob/v2.0.0/mllib/src/main/scala/org/apache/spark/ml/regression/IsotonicRegression.scala")
+case class IsotonicRegressionModel(boundaries: Array[Double],
+                                   predictions: Seq[Double],
+                                   isotonic: Boolean,
+                                   featureIndex: Option[Int]) {
+  def apply(features: Vector): Double = apply(features(featureIndex.get))
+
+  def apply(feature: Double): Double = {
+    val foundIndex = binarySearch(boundaries, feature)
+    val insertIndex = -foundIndex - 1
+
+    // Find if the index was lower than all values,
+    // higher than all values, in between two values or exact match.
+    if (insertIndex == 0) {
+      predictions.head
+    } else if (insertIndex == boundaries.length) {
+      predictions.last
+    } else if (foundIndex < 0) {
+      linearInterpolation(
+        boundaries(insertIndex - 1),
+        predictions(insertIndex - 1),
+        boundaries(insertIndex),
+        predictions(insertIndex),
+        feature)
+    } else {
+      predictions(foundIndex)
+    }
+  }
+
+  private def linearInterpolation(x1: Double, y1: Double, x2: Double, y2: Double, x: Double): Double = {
+    y1 + (y2 - y1) * (x - x1) / (x2 - x1)
+  }
+}
---
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/regression/IsotonicRegression.scala
@@ -0,0 +1,27 @@
+package ml.combust.mleap.runtime.transformer.regression
+
+import ml.combust.mleap.core.regression.IsotonicRegressionModel
+import ml.combust.mleap.runtime.function.UserDefinedFunction
+import ml.combust.mleap.runtime.transformer.Transformer
+import ml.combust.mleap.runtime.transformer.builder.TransformBuilder
+import org.apache.spark.ml.linalg.Vector
+
+import scala.util.Try
+
+/**
+  * Created by hollinwilkins on 12/27/16.
+  */
+case class IsotonicRegression(override val uid: String = Transformer.uniqueName("isotonic_regression"),
+                              featuresCol: String,
+                              predictionCol: String,
+                              model: IsotonicRegressionModel) extends Transformer {
+  val execIndexed: UserDefinedFunction = (features: Vector) => model(features)
+  val exec: UserDefinedFunction = (feature: Double) => model(feature)
+
+  override def transform[TB <: TransformBuilder[TB]](builder: TB): Try[TB] = {
+    model.featureIndex match {
+      case Some(index) => builder.withOutput(predictionCol, featuresCol)(execIndexed)
+      case None => builder.withOutput(predictionCol, featuresCol)(exec)
+    }
+  }
+}

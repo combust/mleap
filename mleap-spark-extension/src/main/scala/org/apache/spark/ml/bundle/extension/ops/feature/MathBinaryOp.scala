11b38d80f450b05d57fb656243d4f9fb20b818b3
Anca Sarb <sarb.anca@gmail.com>
2017-04-26 18:58:56 +0100

    Very small update to use SparkBundleContext instead of MleapContext for these four Spark ops (#194)

--- mleap-spark-extension/src/main/scala/org/apache/spark/ml/bundle/extension/ops/feature/MathBinaryOp.scala
+++ mleap-spark-extension/src/main/scala/org/apache/spark/ml/bundle/extension/ops/feature/MathBinaryOp.scala
@@ -4,27 +4,27 @@ import ml.combust.bundle.BundleContext
 import ml.combust.bundle.dsl._
 import ml.combust.bundle.op.{OpModel, OpNode}
 import ml.combust.mleap.core.feature.{BinaryOperation, MathBinaryModel}
-import ml.combust.mleap.runtime.MleapContext
+import org.apache.spark.ml.bundle.SparkBundleContext
 import org.apache.spark.ml.mleap.feature.MathBinary
 
 /**
   * Created by hollinwilkins on 12/27/16.
   */
-class MathBinaryOp extends OpNode[MleapContext, MathBinary, MathBinaryModel] {
-  override val Model: OpModel[MleapContext, MathBinaryModel] = new OpModel[MleapContext, MathBinaryModel] {
+class MathBinaryOp extends OpNode[SparkBundleContext, MathBinary, MathBinaryModel] {
+  override val Model: OpModel[SparkBundleContext, MathBinaryModel] = new OpModel[SparkBundleContext, MathBinaryModel] {
     override val klazz: Class[MathBinaryModel] = classOf[MathBinaryModel]
 
     override def opName: String = Bundle.BuiltinOps.feature.math_binary
 
     override def store(model: Model, obj: MathBinaryModel)
-                      (implicit context: BundleContext[MleapContext]): Model = {
+                      (implicit context: BundleContext[SparkBundleContext]): Model = {
       model.withAttr("operation", Value.string(obj.operation.name)).
         withAttr("da", obj.da.map(Value.double)).
         withAttr("db", obj.db.map(Value.double))
     }
 
     override def load(model: Model)
-                     (implicit context: BundleContext[MleapContext]): MathBinaryModel = {
+                     (implicit context: BundleContext[SparkBundleContext]): MathBinaryModel = {
       MathBinaryModel(operation = BinaryOperation.forName(model.value("operation").getString),
         da = model.getValue("da").map(_.getDouble),
         db = model.getValue("db").map(_.getDouble))
@@ -38,7 +38,7 @@ class MathBinaryOp extends OpNode[MleapContext, MathBinary, MathBinaryModel] {
   override def model(node: MathBinary): MathBinaryModel = node.model
 
   override def load(node: Node, model: MathBinaryModel)
-                   (implicit context: BundleContext[MleapContext]): MathBinary = {
+                   (implicit context: BundleContext[SparkBundleContext]): MathBinary = {
     val mb = new MathBinary(uid = node.name,
       model = model).setOutputCol(node.shape.standardOutput.name)
     node.shape.getInput("input_a").foreach(a => mb.setInputA(a.name))

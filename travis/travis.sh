88b9dbcadb9d28a1a61e67cff985ddc98befbd36
Mikhail Semeniuk <seme0021@gmail.com>
2017-04-26 14:35:17 -0700

    debugging nosetests exec

--- travis/travis.sh
+++ travis/travis.sh
@@ -5,6 +5,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; the
   source travis/docker.sh
   sbt "+ test" "mleap-serving/test" "+ publishSigned" "mleap-serving/docker:publish"
 else
-  nosetests --exe --where=../ --exclude-dir=./python/mleap/pyspark --verbose
+  pwd
+  nosetests --exe -exclude-dir=./python/mleap/pyspark --verbose
   sbt "+ test" "mleap-serving/test"
 fi

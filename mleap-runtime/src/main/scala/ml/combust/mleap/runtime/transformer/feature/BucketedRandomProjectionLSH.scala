17ae95b3dd4073c87d61801c036370668a83abd2
Anca Sarb <sarb.anca@gmail.com>
2017-04-28 17:16:20 +0100

    add schema for transformers containing seq of tensors

--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/BucketedRandomProjectionLSH.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/BucketedRandomProjectionLSH.scala
@@ -5,6 +5,9 @@ import ml.combust.mleap.runtime.function.UserDefinedFunction
 import ml.combust.mleap.runtime.transformer.FeatureTransformer
 import ml.combust.mleap.tensor.Tensor
 import ml.combust.mleap.core.util.VectorConverters._
+import ml.combust.mleap.runtime.types._
+
+import scala.util.{Success, Try}
 
 /**
   * Created by hollinwilkins on 12/28/16.
@@ -14,4 +17,9 @@ case class BucketedRandomProjectionLSH(override val uid: String,
                                        override val outputCol: String,
                                        model: BucketedRandomProjectionLSHModel) extends FeatureTransformer {
   override val exec: UserDefinedFunction = (features: Tensor[Double]) => model(features).map(v => v: Tensor[Double])
+
+  override def getSchema(): Try[Seq[StructField]] = Success(Seq(
+    StructField(inputCol, TensorType(DoubleType())),
+    StructField(outputCol, ListType(TensorType(DoubleType())))
+  ))
 }

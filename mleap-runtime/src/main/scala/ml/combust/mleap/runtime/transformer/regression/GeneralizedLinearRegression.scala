0f706b4207f4b5806bae4f751b11f45a0fa22f7f
Anca Sarb <sarb.anca@gmail.com>
2017-05-02 22:50:45 +0100

    add GeneralizedLinearRegression transformer and test

--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/regression/GeneralizedLinearRegression.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/regression/GeneralizedLinearRegression.scala
@@ -6,8 +6,9 @@ import ml.combust.mleap.runtime.transformer.Transformer
 import ml.combust.mleap.runtime.transformer.builder.TransformBuilder
 import ml.combust.mleap.tensor.Tensor
 import ml.combust.mleap.core.util.VectorConverters._
+import ml.combust.mleap.runtime.types.{DoubleType, StructField, TensorType}
 
-import scala.util.Try
+import scala.util.{Success, Try}
 
 /**
   * Created by hollinwilkins on 12/28/16.
@@ -26,6 +27,18 @@ case class GeneralizedLinearRegression(override val uid: String = Transformer.un
         for(b1 <- builder.withOutput(predictionCol, featuresCol)(predict);
             b2 <- b1.withOutput(col, featuresCol)(predictLink)) yield b2
       case None => builder.withOutput(predictionCol, featuresCol)(predict)
+    }
+  }
+
+  override def getSchema(): Try[Seq[StructField]] = {
+    linkPredictionCol match {
+      case Some(col) => Success(Seq(
+        StructField(featuresCol, TensorType(DoubleType())),
+        StructField(predictionCol, DoubleType()),
+        StructField(col, DoubleType())))
+      case None => Success(Seq(
+        StructField(featuresCol, TensorType(DoubleType())),
+        StructField(predictionCol, DoubleType())))
     }
   }
 }

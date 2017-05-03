bb80c2aa49480909feb92951c3ebff5dc67e3e53
Anca Sarb <sarb.anca@gmail.com>
2017-05-03 08:09:13 +0100

    adding NaiveBayesClassifier transformer and test

--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/classification/NaiveBayesClassifier.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/classification/NaiveBayesClassifier.scala
@@ -6,8 +6,9 @@ import ml.combust.mleap.runtime.transformer.Transformer
 import ml.combust.mleap.runtime.transformer.builder.TransformBuilder
 import ml.combust.mleap.tensor.Tensor
 import ml.combust.mleap.core.util.VectorConverters._
+import ml.combust.mleap.runtime.types.{DoubleType, StructField, TensorType}
 
-import scala.util.Try
+import scala.util.{Success, Try}
 
 /**
   * Created by fshabbir on 12/24/16.
@@ -39,6 +40,23 @@ case class NaiveBayesClassifier(override val uid: String = Transformer.uniqueNam
             b2 <- b.withOutput(predictionCol, p)(probabilityToPrediction)) yield b2
       case (None, None) =>
         builder.withOutput(predictionCol, featuresCol)(predict)
+    }
+  }
+
+  override def getSchema(): Try[Seq[StructField]] = {
+    (rawPredictionCol, probabilityCol) match {
+      case ((Some(rp), Some(p))) => Success(Seq(StructField(featuresCol, TensorType(DoubleType())),
+                                                StructField(rp, TensorType(DoubleType())),
+                                                StructField(p, TensorType(DoubleType())),
+                                                StructField(predictionCol, DoubleType())))
+      case ((Some(rp), None)) => Success(Seq(StructField(featuresCol, TensorType(DoubleType())),
+                                              StructField(rp, TensorType(DoubleType())),
+                                              StructField(predictionCol, DoubleType())))
+      case (None, Some(p)) => Success(Seq(StructField(featuresCol, TensorType(DoubleType())),
+                                          StructField(p, TensorType(DoubleType())),
+                                          StructField(predictionCol, DoubleType())))
+      case (None, None) => Success(Seq(StructField(featuresCol, TensorType(DoubleType())),
+                                      StructField(predictionCol, DoubleType())))
     }
   }
 }

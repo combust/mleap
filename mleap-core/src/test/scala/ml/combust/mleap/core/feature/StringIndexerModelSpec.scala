89d6ca5fe14cd7f1f32ebef3e0f8a15ff221ccd2
Oleksii Lomako <alexey.lomako@gmail.com>
2017-06-09 19:57:57 +0300

    add "keep" as a invalid handling mode to StringIndexerModel (#226)

--- mleap-core/src/test/scala/ml/combust/mleap/core/feature/StringIndexerModelSpec.scala
+++ mleap-core/src/test/scala/ml/combust/mleap/core/feature/StringIndexerModelSpec.scala
@@ -1,11 +1,12 @@
 package ml.combust.mleap.core.feature
 
 import org.scalatest.FunSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
 
 /**
   * Created by hwilkins on 1/21/16.
   */
-class StringIndexerModelSpec extends FunSpec {
+class StringIndexerModelSpec extends FunSpec with TableDrivenPropertyChecks {
   describe("#apply") {
     it("returns the index of the string") {
       val indexer = StringIndexerModel(Array("hello", "there", "dude"))
@@ -13,6 +14,45 @@ class StringIndexerModelSpec extends FunSpec {
       assert(indexer("hello") == 0.0)
       assert(indexer("there") == 1.0)
       assert(indexer("dude") == 2.0)
+    }
+
+    it("returns the index of Optional string") {
+      val indexer = StringIndexerModel(Array("hello", "there", "dude"))
+
+      assert(indexer(Some("hello")) == 0.0)
+      assert(indexer(Some("there")) == 1.0)
+      assert(indexer(Some("dude")) == 2.0)
+    }
+
+    it("throws NullPointerException when encounters NULL/None and handleInvalid is not keep") {
+      val indexer = StringIndexerModel(Array("hello"))
+      val nullLabels = Table(null, None)
+
+      forAll(nullLabels) { (label: Any) =>
+        intercept[NullPointerException] {
+          indexer(label)
+        }
+      }
+    }
+
+    it("throws NoSuchElementException when encounters unseen label and handleInvalid is not keep") {
+      val indexer = StringIndexerModel(Array("hello"))
+      val unseenLabels = Table("unknown1", Some("unknown2"))
+
+      forAll(unseenLabels) { (label: Any) =>
+        intercept[NoSuchElementException] {
+          indexer(label)
+        }
+      }
+    }
+
+    it("returns default index for HandleInvalid.keep mode") {
+      val indexer = StringIndexerModel(Array("hello", "there", "dude"), HandleInvalid.Keep)
+      val invalidLabels = Table("unknown", Some("other unknown"), null, None)
+
+      forAll(invalidLabels) { (label: Any) =>
+        assert(indexer(label) == 3.0)
+      }
     }
   }
 }

a88f156a0a4264ed6153d0022ed526b8f1118f5a
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-06-30 13:04:42 -0700

    Boot with model loaded.

--- mleap-serving/src/main/scala/ml/combust/mleap/serving/Boot.scala
+++ mleap-serving/src/main/scala/ml/combust/mleap/serving/Boot.scala
@@ -6,6 +6,10 @@ import akka.actor.ActorSystem
   * Created by hollinwilkins on 1/30/17.
   */
 object Boot extends App {
+  if (args.length == 1) {
+    sys.props("ml.combust.mleap.serving.model") = args(0)
+  }
+
   val system = ActorSystem("MleapServing")
   MleapServer(system)
 }

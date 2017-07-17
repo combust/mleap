61ca81016eeee94ee36c6a0fa745a03b97b14216
Viktor Sobol <viktor.sobol@mbp15168.tw.com>
2017-07-17 14:09:58 +0300

    [FRAUD-1115] update spark revision to 2.2.0 release

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ import Keys._
 object Dependencies {
   import DependencyHelpers._
 
-  val sparkVersion = "2.2.0-SNAPSHOT"
+  val sparkVersion = "2.2.0"
   val scalaTestVersion = "3.0.0"
   val tensorflowVersion = "1.1.0"
   val akkaVersion = "2.4.16"

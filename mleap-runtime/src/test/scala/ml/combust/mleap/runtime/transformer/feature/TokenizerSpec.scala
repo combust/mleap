aaa7174d518cd81281e3d2874bcea75f80f616d9
Anca Sarb <sarb.anca@gmail.com>
2017-07-25 12:17:53 +0100

    fix tokenizer spec

--- mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/TokenizerSpec.scala
+++ mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/TokenizerSpec.scala
@@ -1,5 +1,6 @@
 package ml.combust.mleap.runtime.transformer.feature
 
+import ml.combust.mleap.core.feature.TokenizerModel
 import ml.combust.mleap.core.types._
 import org.scalatest.FunSpec
 
@@ -8,7 +9,7 @@ class TokenizerSpec extends FunSpec {
   describe("#getFields") {
     it("has the correct inputs and outputs") {
       val transformer = Tokenizer(shape = NodeShape().withStandardInput("input").
-              withStandardOutput("output"))
+              withStandardOutput("output"), model = TokenizerModel.defaultTokenizer)
       assert(transformer.schema.fields ==
         Seq(StructField("input", ScalarType.String),
           StructField("output", ListType(BasicType.String))))

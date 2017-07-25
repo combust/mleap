4e8a4db502a2bda860a336105ab972ac3cfca703
Anca Sarb <sarb.anca@gmail.com>
2017-07-25 11:38:34 +0100

    migrate ovr to SimpleSparkOp

--- mleap-spark-extension/src/main/scala/org/apache/spark/ml/bundle/extension/ops/classification/OneVsRestOp.scala
+++ mleap-spark-extension/src/main/scala/org/apache/spark/ml/bundle/extension/ops/classification/OneVsRestOp.scala
@@ -2,17 +2,17 @@ package org.apache.spark.ml.bundle.extension.ops.classification
 
 import ml.combust.bundle.BundleContext
 import ml.combust.bundle.dsl._
-import ml.combust.bundle.op.{OpModel, OpNode}
+import ml.combust.bundle.op.OpModel
 import ml.combust.bundle.serializer.ModelSerializer
 import org.apache.spark.ml.attribute.NominalAttribute
-import org.apache.spark.ml.bundle.SparkBundleContext
+import org.apache.spark.ml.bundle.{ParamSpec, SimpleParamSpec, SimpleSparkOp, SparkBundleContext}
 import org.apache.spark.ml.classification.ClassificationModel
 import org.apache.spark.ml.mleap.classification.OneVsRestModel
 
 /**
   * Created by hollinwilkins on 8/21/16.
   */
-class OneVsRestOp extends OpNode[SparkBundleContext, OneVsRestModel, OneVsRestModel] {
+class OneVsRestOp extends SimpleSparkOp[OneVsRestModel] {
   override val Model: OpModel[SparkBundleContext, OneVsRestModel] = new OpModel[SparkBundleContext, OneVsRestModel] {
     override val klazz: Class[OneVsRestModel] = classOf[OneVsRestModel]
 
@@ -47,28 +47,18 @@ class OneVsRestOp extends OpNode[SparkBundleContext, OneVsRestModel, OneVsRestMo
     }
   }
 
-  override val klazz: Class[OneVsRestModel] = classOf[OneVsRestModel]
+  override def sparkInputs(obj: OneVsRestModel): Seq[ParamSpec] = Seq("features" -> obj.featuresCol)
 
-  override def name(node: OneVsRestModel): String = node.uid
+  override def sparkOutputs(obj: OneVsRestModel): Seq[SimpleParamSpec] = Seq("prediction" -> obj.predictionCol)
 
-  override def model(node: OneVsRestModel): OneVsRestModel = node
-
-  override def load(node: Node, model: OneVsRestModel)
-                   (implicit context: BundleContext[SparkBundleContext]): OneVsRestModel = {
-    assert(node.shape.getOutput("probability").isEmpty, "default OneVsRestModel does not support probability columns, use mleap-spark-extension library")
-
+  override def sparkLoad(uid: String, shape: NodeShape, model: OneVsRestModel): OneVsRestModel = {
     val labelMetadata = NominalAttribute.defaultAttr.
-      withName(node.shape.output("prediction").name).
+      withName(shape.output("prediction").name).
       withNumValues(model.models.length).
       toMetadata
-    val ovr = new OneVsRestModel(uid = node.name, models = model.models, labelMetadata = labelMetadata).
-      setFeaturesCol(node.shape.input("features").name).
-      setPredictionCol(node.shape.output("prediction").name)
-    ovr.get(ovr.probabilityCol).foreach(ovr.setProbabilityCol)
-    ovr
-  }
 
-  override def shape(node: OneVsRestModel): NodeShape = NodeShape().withInput(node.getFeaturesCol, "features").
-    withOutput(node.getPredictionCol, "prediction").
-    withOutput(node.get(node.probabilityCol), "probability")
+    new OneVsRestModel(uid = uid,
+      labelMetadata = labelMetadata,
+      models = model.models)
+  }
 }

1b6db0073a51f8493ada5db2586cc71f495665f7
Anca Sarb <sarb.anca@gmail.com>
2017-07-28 13:05:40 +0100

    add input/output schema for VectorIndexerModel

--- mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/VectorIndexerSpec.scala
+++ mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/VectorIndexerSpec.scala
@@ -1,17 +1,19 @@
 package ml.combust.mleap.runtime.transformer.feature
 
+import ml.combust.mleap.core.feature.VectorIndexerModel
 import ml.combust.mleap.core.types._
 import org.scalatest.FunSpec
 
 class VectorIndexerSpec extends FunSpec {
 
-  describe("#getFields") {
+  describe("input/output schema") {
     it("has the correct inputs and outputs") {
-      val transformer = VectorIndexer(shape = NodeShape.vector(3, 20), model = null)
+      val transformer = VectorIndexer(shape = NodeShape.vector(3, 20),
+        model = VectorIndexerModel(3, Map()))
 
       assert(transformer.schema.fields ==
-        Seq(StructField("input", TensorType(BasicType.Double, Seq(3))),
-          StructField("output", TensorType(BasicType.Double, Seq(20)))))
+        Seq(StructField("input", TensorType.Double()),
+          StructField("output", TensorType.Double())))
     }
   }
 }

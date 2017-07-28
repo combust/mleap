63cc48a44ce4142a0e834e18d5e68ce75c7b0628
Anca Sarb <sarb.anca@gmail.com>
2017-07-28 13:13:03 +0100

    add input/output schema for WordToVectorModel

--- mleap-core/src/test/scala/ml/combust/mleap/core/feature/WordToVectorModelSpec.scala
+++ mleap-core/src/test/scala/ml/combust/mleap/core/feature/WordToVectorModelSpec.scala
@@ -1,16 +1,16 @@
 package ml.combust.mleap.core.feature
 
-import ml.combust.mleap.core.types.{StructField, TensorType}
+import ml.combust.mleap.core.types.{BasicType, ListType, StructField, TensorType}
 import org.scalatest.FunSpec
 
-class VectorSlicerModelSpec extends FunSpec {
+class WordToVectorModelSpec extends FunSpec {
 
-  describe("vector slicer model") {
-    val model = VectorSlicerModel(Array())
+  describe("word to vector model") {
+    val model = WordToVectorModel(Map("test" -> 1), Array(12))
 
     it("has the right input schema") {
       assert(model.inputSchema.fields ==
-        Seq(StructField("input", TensorType.Double())))
+        Seq(StructField("input", ListType(BasicType.String))))
     }
 
     it("has the right output schema") {
--- mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/WordToVectorSpec.scala
+++ mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/WordToVectorSpec.scala
@@ -1,18 +1,20 @@
 package ml.combust.mleap.runtime.transformer.feature
 
+import ml.combust.mleap.core.feature.WordToVectorModel
 import ml.combust.mleap.core.types._
 import org.scalatest.FunSpec
 
 class WordToVectorSpec extends FunSpec {
 
-  describe("#getFields") {
+  describe("input/output schema") {
     it("has the correct inputs and outputs") {
       val transformer = WordToVector(shape = NodeShape().withStandardInput("input").
-              withStandardOutput("output"), model = null)
+              withStandardOutput("output"),
+              model =  WordToVectorModel(Map("test" -> 1), Array(12)))
 
       assert(transformer.schema.fields ==
         Seq(StructField("input", ListType(BasicType.String)),
-          StructField("output", TensorType(BasicType.Double, Seq(4)))))
+          StructField("output", TensorType.Double())))
     }
   }
 }

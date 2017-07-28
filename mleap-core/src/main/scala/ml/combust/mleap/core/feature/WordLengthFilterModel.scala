6c56e10d12ee2eea1435e207ca2415eaaed268fb
Anca Sarb <sarb.anca@gmail.com>
2017-07-28 12:32:39 +0100

    add input/output schema for WordLengthFilterModel

--- mleap-core/src/main/scala/ml/combust/mleap/core/feature/WordLengthFilterModel.scala
+++ mleap-core/src/main/scala/ml/combust/mleap/core/feature/WordLengthFilterModel.scala
@@ -1,6 +1,7 @@
 package ml.combust.mleap.core.feature
 
 import ml.combust.mleap.core.Model
+import ml.combust.mleap.core.types.{BasicType, ListType, StructType}
 
 /**
   * Created by mageswarand on 14/2/17.
@@ -12,4 +13,8 @@ case class WordLengthFilterModel(length: Int = 3) extends Model {
       word.forall(w =>
         w.isLetter) && word.length > length)
   }
+
+  override def inputSchema: StructType = StructType("input" -> ListType(BasicType.String)).get
+
+  override def outputSchema: StructType = StructType("output" -> ListType(BasicType.String)).get
 }

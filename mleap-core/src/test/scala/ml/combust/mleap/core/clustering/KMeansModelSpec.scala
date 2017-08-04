3ce30d79b50e0ac1a39877b7c1e552060317c58c
Anca Sarb <sarb.anca@gmail.com>
2017-08-04 01:25:17 +0100

    input/output schema for KMeans models

--- mleap-core/src/test/scala/ml/combust/mleap/core/clustering/KMeansModelSpec.scala
+++ mleap-core/src/test/scala/ml/combust/mleap/core/clustering/KMeansModelSpec.scala
@@ -1,5 +1,6 @@
 package ml.combust.mleap.core.clustering
 
+import ml.combust.mleap.core.types.{ScalarType, StructField, TensorType}
 import org.apache.spark.ml.linalg.Vectors
 import org.scalatest.FunSpec
 
@@ -17,6 +18,16 @@ class KMeansModelSpec extends FunSpec {
       assert(km(Vectors.dense(Array(2.0, 5.0, 34.0))) == 0)
       assert(km(Vectors.dense(Array(20.0, 230.0, 34.0))) == 1)
       assert(km(Vectors.dense(Array(111.0, 20.0, 56.0))) == 2)
+    }
+  }
+
+  describe("input/output schema") {
+    it("has the right input schema") {
+      assert(km.inputSchema.fields == Seq(StructField("features", TensorType.Double())))
+    }
+
+    it("has the right output schema") {
+      assert(km.outputSchema.fields == Seq(StructField("prediction", ScalarType.Int)))
     }
   }
 }
--- mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/clustering/BisectingKMeansSpec.scala
+++ mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/clustering/BisectingKMeansSpec.scala
@@ -1,15 +1,21 @@
 package ml.combust.mleap.runtime.transformer.clustering
 
+import ml.combust.mleap.core.clustering.{BisectingKMeansModel, ClusteringTreeNode}
 import ml.combust.mleap.core.types._
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.linalg.mleap.VectorWithNorm
 import org.scalatest.FunSpec
 
 class BisectingKMeansSpec extends FunSpec {
-  describe("#getFields") {
+
+  describe("input/output schema") {
     it("has the correct inputs and outputs") {
-      val transformer = BisectingKMeans(shape = NodeShape.basicCluster(3), model = null)
+      val transformer = BisectingKMeans(shape = NodeShape.basicCluster(3),
+        model = new BisectingKMeansModel(ClusteringTreeNode(23,
+          VectorWithNorm(Vectors.dense(1, 2, 3)) , Array())))
 
       assert(transformer.schema.fields ==
-        Seq(StructField("features", TensorType(BasicType.Double, Seq(3))),
+        Seq(StructField("features", TensorType.Double(3)),
           StructField("prediction", ScalarType.Int)))
     }
   }

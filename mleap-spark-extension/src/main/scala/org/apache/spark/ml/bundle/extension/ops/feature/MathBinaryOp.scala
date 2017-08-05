8231f05246187c9cd140f0ace1661da5b9173328
Anca Sarb <sarb.anca@gmail.com>
2017-08-05 22:36:30 +0100

    fix more parity specs

--- mleap-spark-extension/src/main/scala/org/apache/spark/ml/bundle/extension/ops/feature/MathBinaryOp.scala
+++ mleap-spark-extension/src/main/scala/org/apache/spark/ml/bundle/extension/ops/feature/MathBinaryOp.scala
@@ -50,11 +50,11 @@ class MathBinaryOp extends OpNode[SparkBundleContext, MathBinary, MathBinaryMode
     var shape = NodeShape().withStandardOutput(node.getOutputCol)
 
     if(node.isSet(node.inputA)) {
-      shape = shape.withInput(node.getInputA, "input_a")
+      shape = shape.withInput("input_a", node.getInputA)
     }
 
     if(node.isSet(node.inputB)) {
-      shape = shape.withInput(node.getInputB, "input_b")
+      shape = shape.withInput("input_b", node.getInputB)
     }
 
     shape

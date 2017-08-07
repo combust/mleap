a178229445e54bf1321e98ab68e2e5b5c59a7124
Anca Sarb <sarb.anca@gmail.com>
2017-08-07 23:54:20 +0100

    fix ChiSqSelectorOp parity spec

--- mleap-spark/src/main/scala/org/apache/spark/ml/bundle/ops/feature/ChiSqSelectorOp.scala
+++ mleap-spark/src/main/scala/org/apache/spark/ml/bundle/ops/feature/ChiSqSelectorOp.scala
@@ -2,11 +2,12 @@ package org.apache.spark.ml.bundle.ops.feature
 
 import ml.combust.bundle.BundleContext
 import ml.combust.bundle.dsl._
-import ml.combust.bundle.op.{OpModel, OpNode}
+import ml.combust.bundle.op.OpModel
+import ml.combust.mleap.core.types.TensorShape
 import org.apache.spark.ml.bundle.{ParamSpec, SimpleParamSpec, SimpleSparkOp, SparkBundleContext}
 import org.apache.spark.ml.feature.ChiSqSelectorModel
-import org.apache.spark.ml.param.Param
 import org.apache.spark.mllib.feature
+import org.apache.spark.sql.mleap.TypeConverters.sparkToMleapDataShape
 
 /**
   * Created by hollinwilkins on 12/27/16.
@@ -19,7 +20,12 @@ class ChiSqSelectorOp extends SimpleSparkOp[ChiSqSelectorModel] {
 
     override def store(model: Model, obj: ChiSqSelectorModel)
                       (implicit context: BundleContext[SparkBundleContext]): Model = {
+
+      val dataset = context.context.dataset.get
+      val inputShape = sparkToMleapDataShape(dataset.schema(obj.getFeaturesCol)).asInstanceOf[TensorShape]
+
       model.withValue("filter_indices", Value.longList(obj.selectedFeatures.map(_.toLong).toSeq))
+           .withValue("input_size", Value.int(inputShape.dimensions.get(0)))
     }
 
     override def load(model: Model)

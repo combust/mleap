88a45ae3966687720e577db64832ad3bb3bd711e
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-08-29 00:30:47 -0700

    Automatically set spark registry version based on version string.

--- mleap-spark-base/src/main/scala/org/apache/spark/ml/bundle/SparkBundleContext.scala
+++ mleap-spark-base/src/main/scala/org/apache/spark/ml/bundle/SparkBundleContext.scala
@@ -8,13 +8,26 @@ import org.apache.spark.sql.DataFrame
   * Created by hollinwilkins on 10/26/16.
   */
 object SparkBundleContext {
+  private val sparkVersion = org.apache.spark.SPARK_VERSION
+  private val versionedRegistryKey: String = {
+    if(sparkVersion.startsWith("2.0")) {
+      "ml.combust.mleap.spark.registry.v20"
+    } else if(sparkVersion.startsWith("2.1")) {
+      "ml.combust.mleap.spark.registry.v21"
+    } else if(sparkVersion.startsWith("2.2")) {
+      "ml.combust.mleap.spark.registry.v22"
+    } else {
+      throw new IllegalStateException(s"unsupported Spark version: $sparkVersion")
+    }
+  }
+
   implicit lazy val defaultContext: SparkBundleContext = SparkBundleContext(None, Some(classOf[SparkBundleContext].getClassLoader))
 
   def apply(dataset: Option[DataFrame] = None): SparkBundleContext = apply(dataset, None)
 
   def apply(dataset: Option[DataFrame], clOption: Option[ClassLoader]): SparkBundleContext = {
     val cl = clOption.getOrElse(ClassLoaderUtil.findClassLoader(classOf[SparkBundleContext].getCanonicalName))
-    apply(dataset, BundleRegistry("ml.combust.mleap.spark.registry.default", Some(cl)))
+    apply(dataset, BundleRegistry(versionedRegistryKey, Some(cl)))
   }
 }
 

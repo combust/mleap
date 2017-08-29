f76b77bbee40b1b3a7cd60259b916afdc0b27e40
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-08-28 23:33:29 -0700

    Make sure that users provide a transformed data frame.

--- mleap-spark/src/main/scala/org/apache/spark/ml/bundle/SimpleSparkOp.scala
+++ mleap-spark/src/main/scala/org/apache/spark/ml/bundle/SimpleSparkOp.scala
@@ -30,7 +30,27 @@ abstract class SimpleSparkOp[N <: Transformer](implicit ct: ClassTag[N]) extends
 
   override def shape(node: N)
                     (implicit context: BundleContext[SparkBundleContext]): NodeShape = {
-    SparkShapeSaver(context.context.dataset.get,
+    val dataset = context.context.dataset.getOrElse {
+      throw new IllegalArgumentException(
+        """
+          |Must provide a transformed data frame to MLeap for serializing a pipeline.
+          |The transformed data frame is used to extract data types and other metadata
+          |required for execution.
+          |
+          |Example usage:
+          |```
+          |val sparkTransformer: org.apache.spark.ml.Transformer
+          |val transformedDataset = sparkTransformer.transform(trainingDataset)
+          |
+          |implicit val sbc = SparkBundleContext().withDataset(transformedDataset)
+          |
+          |for(bf <- managed(BundleFile(file))) {
+          |  sparkTransformer.writeBundle.format(SerializationFormat.Json).save(bf).get
+          |}
+          |```
+        """.stripMargin)
+    }
+    SparkShapeSaver(dataset,
       node,
       sparkInputs(node),
       sparkOutputs(node)).asNodeShape

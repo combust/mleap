0c3a4d51c7410e2a6694d67c8c2a5fd5e1020e6d
smorring <32274923+smorring@users.noreply.github.com>
2017-09-26 22:20:38 -0500

    Fix issue with MaxAbsScaler (#274)

--- mleap-core/src/main/scala/ml/combust/mleap/core/feature/MaxAbsScalerModel.scala
+++ mleap-core/src/main/scala/ml/combust/mleap/core/feature/MaxAbsScalerModel.scala
@@ -35,7 +35,7 @@ case class MaxAbsScalerModel(maxAbs: Vector) extends Model {
         val nnz = vs.length
         var i = 0
         while (i < nnz) {
-          val raw = max(-1.0, min(1.0, values(i) / maxAbsUnzero(i)))
+          val raw = max(-1.0, min(1.0, values(i) / maxAbsUnzero(indices(i))))
 
           vs(i) = raw
           i += 1

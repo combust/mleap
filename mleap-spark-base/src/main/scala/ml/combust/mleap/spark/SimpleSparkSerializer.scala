020e14338501885d19b5a6796361e0df86fc3c2a
Cory Locklear <corylocklear@gmail.com>
2017-10-03 18:22:38 -0400

    Allow serialization format to be set in SimpleSparkSerializer (#282)
    * Allow serialization format to be set in SimpleSparkSerializer
    
    * Create a second 'serializeToBundle' function that takes a format parameter

--- mleap-spark-base/src/main/scala/ml/combust/mleap/spark/SimpleSparkSerializer.scala
+++ mleap-spark-base/src/main/scala/ml/combust/mleap/spark/SimpleSparkSerializer.scala
@@ -2,6 +2,7 @@ package ml.combust.mleap.spark
 
 import ml.combust.bundle.BundleFile
 import ml.combust.mleap.spark.SparkSupport._
+import ml.combust.bundle.serializer.SerializationFormat
 import org.apache.spark.ml.Transformer
 import org.apache.spark.ml.bundle.SparkBundleContext
 import org.apache.spark.sql.DataFrame
@@ -13,12 +14,16 @@ import resource._
   */
 class SimpleSparkSerializer() {
   def serializeToBundle(transformer: Transformer, path: String, dataset: DataFrame): Unit = {
+    serializeToBundleWithFormat(transformer = transformer, path = path, dataset = dataset, format = SerializationFormat.Json)
+  }
+
+  def serializeToBundleWithFormat(transformer: Transformer, path: String, dataset: DataFrame, format: SerializationFormat = SerializationFormat.Json): Unit = {
     implicit val context: SparkBundleContext = Option(dataset).
       map(d => SparkBundleContext.defaultContext.withDataset(d)).
       getOrElse(SparkBundleContext.defaultContext)
 
     (for(file <- managed(BundleFile(path))) yield {
-      transformer.writeBundle.save(file).get
+      transformer.writeBundle.format(format).save(file).get
     }).tried.get
   }
 

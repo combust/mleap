2382ac72cc5a7386fb102ee67410f028e0a76fc0
Hollin Wilkins <hollinrwilkins@gmail.com>
2017-10-10 18:17:36 -0700

    Implement skip logic for string indexer.

--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/StringIndexer.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/StringIndexer.scala
@@ -1,15 +1,30 @@
 package ml.combust.mleap.runtime.transformer.feature
 
-import ml.combust.mleap.core.feature.StringIndexerModel
+import ml.combust.mleap.core.feature.{HandleInvalid, StringIndexerModel}
 import ml.combust.mleap.core.types._
-import ml.combust.mleap.runtime.function.UserDefinedFunction
-import ml.combust.mleap.runtime.frame.{SimpleTransformer, Transformer}
+import ml.combust.mleap.runtime.function.{FieldSelector, UserDefinedFunction}
+import ml.combust.mleap.runtime.frame.{FrameBuilder, Transformer}
+
+import scala.util.Try
 
 /**
   * Created by hwilkins on 10/22/15.
   */
 case class StringIndexer(override val uid: String = Transformer.uniqueName("string_indexer"),
                          override val shape: NodeShape,
-                         override val model: StringIndexerModel) extends SimpleTransformer {
+                         override val model: StringIndexerModel) extends Transformer {
+  val input: String = inputSchema.fields.head.name
+  val inputSelector: FieldSelector = input
+  val output: String = outputSchema.fields.head.name
   val exec: UserDefinedFunction = (value: String) => model(value).toDouble
+
+  override def transform[FB <: FrameBuilder[FB]](builder: FB): Try[FB] = {
+    if(model.handleInvalid == HandleInvalid.Skip) {
+      builder.filter(input) {
+        (key: String) => model.stringToIndex.contains(key)
+      }.flatMap(_.withColumn(output, inputSelector)(exec))
+    } else {
+      builder.withColumn(output, inputSelector)(exec)
+    }
+  }
 }

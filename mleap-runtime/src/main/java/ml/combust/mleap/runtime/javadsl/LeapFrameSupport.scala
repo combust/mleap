158b529fc7b01ce08502a97c175519dfad0fbf9e
Anca Sarb <sarb.anca@gmail.com>
2017-11-17 17:24:05 +0000

    nicer support for leap frame operations in Java

---
+++ mleap-runtime/src/main/java/ml/combust/mleap/runtime/javadsl/LeapFrameSupport.scala
@@ -0,0 +1,20 @@
+package ml.combust.mleap.runtime.javadsl
+
+import ml.combust.mleap.runtime.frame.{DefaultLeapFrame, Row}
+
+import scala.collection.JavaConverters._
+
+class LeapFrameSupport {
+
+  def collect(frame: DefaultLeapFrame): java.util.List[Row] = {
+    frame.collect().asJava
+  }
+
+  def select(frame: DefaultLeapFrame, fieldNames: java.util.List[String]): DefaultLeapFrame = {
+    frame.select(fieldNames.asScala: _*).get
+  }
+
+  def drop(frame: DefaultLeapFrame, names: java.util.List[String]): DefaultLeapFrame = {
+    frame.drop(names.asScala: _*).get
+  }
+}

5cfce27670d357540f4da1342ebc93672cbfcd2d
Hollin Wilkins <hollinrwilkins@gmail.com>
2018-03-28 08:10:44 -0700

    Update tensorflow and include JNI. (#349)

--- mleap-tensorflow/src/main/scala/ml/combust/mleap/tensorflow/converter/TensorflowConverter.scala
+++ mleap-tensorflow/src/main/scala/ml/combust/mleap/tensorflow/converter/TensorflowConverter.scala
@@ -11,27 +11,28 @@ import org.tensorflow
   */
 object TensorflowConverter {
   def convert(tensor: tensorflow.Tensor[_], tt: TensorType): DenseTensor[_] = {
-    val dimensions = tensor.shape().map(_.toInt)
+    val size = tensor.shape().product.toInt
+    val dimensions = tt.dimensions.get
 
     tt.base match {
       case BasicType.Byte =>
-        val b = ByteBuffer.allocate(Math.max(1, dimensions.product))
+        val b = ByteBuffer.allocate(Math.max(1, size))
         tensor.writeTo(b)
         DenseTensor(b.array(), dimensions)
       case BasicType.Int =>
-        val b = IntBuffer.allocate(Math.max(1, dimensions.product))
+        val b = IntBuffer.allocate(Math.max(1, size))
         tensor.writeTo(b)
         DenseTensor(b.array(), dimensions)
       case BasicType.Long =>
-        val b = LongBuffer.allocate(Math.max(1, dimensions.product))
+        val b = LongBuffer.allocate(Math.max(1, size))
         tensor.writeTo(b)
         DenseTensor(b.array(), dimensions)
       case BasicType.Float =>
-        val b = FloatBuffer.allocate(Math.max(1, dimensions.product))
+        val b = FloatBuffer.allocate(Math.max(1, size))
         tensor.writeTo(b)
         DenseTensor(b.array(), dimensions)
       case BasicType.Double =>
-        val b = DoubleBuffer.allocate(Math.max(1, dimensions.product))
+        val b = DoubleBuffer.allocate(Math.max(1, size))
         tensor.writeTo(b)
         DenseTensor(b.array(), dimensions)
       case _ =>

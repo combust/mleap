9cc556b53cc8a41e61b353b63e32ce4436fe9486
Hollin Wilkins <hollinrwilkins@gmail.com>
2018-03-28 07:40:06 -0700

    Improve reverse string indexer with list inputs/outputs.

--- mleap-core/src/main/scala/ml/combust/mleap/core/feature/ReverseStringIndexerModel.scala
+++ mleap-core/src/main/scala/ml/combust/mleap/core/feature/ReverseStringIndexerModel.scala
@@ -1,16 +1,23 @@
 package ml.combust.mleap.core.feature
 
 import ml.combust.mleap.core.Model
-import ml.combust.mleap.core.types.{ScalarType, StructType}
+import ml.combust.mleap.core.types._
 
 /** Class for a reverse string indexer model.
   *
   * This model reverses the [[StringIndexerModel]] model.
+  *
   * Use this to go from an integer representation of a label to a string.
+  * Alternatively, goes from a list of integers to a list of labels.
   *
   * @param labels labels for reverse string indexing
+  * @param inputShape shape of the input, determines scalar/list output as well
   */
-case class ReverseStringIndexerModel(labels: Seq[String]) extends Model {
+case class ReverseStringIndexerModel(labels: Seq[String],
+                                     inputShape: DataShape = ScalarShape(false)) extends Model {
+  require(inputShape.isScalar || inputShape.isList, "must be scalar or list input type")
+  require(inputShape.nonNullable, "cannot take null inputs")
+
   private val indexToString: Map[Int, String] = labels.zipWithIndex.map(v => (v._2, v._1)).toMap
 
   /** Map an index to its string representation.
@@ -20,7 +27,14 @@ case class ReverseStringIndexerModel(labels: Seq[String]) extends Model {
     */
   def apply(index: Int): String = indexToString(index)
 
-  override def inputSchema: StructType = StructType("input" -> ScalarType.Double.nonNullable).get
+  /** Map a list of indices to string representations.
+    *
+    * @param indices sequence of indices
+    * @return sequence of labels
+    */
+  def apply(indices: Seq[Int]): Seq[String] = indices.map(indexToString)
 
-  override def outputSchema: StructType = StructType("output" -> ScalarType.String).get
+  override def inputSchema: StructType = StructType("input" -> DataType(BasicType.Double, inputShape)).get
+
+  override def outputSchema: StructType = StructType("output" -> DataType(BasicType.String, inputShape).asNullable).get
 }
--- mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/ReverseStringIndexer.scala
+++ mleap-runtime/src/main/scala/ml/combust/mleap/runtime/transformer/feature/ReverseStringIndexer.scala
@@ -1,7 +1,7 @@
 package ml.combust.mleap.runtime.transformer.feature
 
 import ml.combust.mleap.core.feature.ReverseStringIndexerModel
-import ml.combust.mleap.core.types.NodeShape
+import ml.combust.mleap.core.types.{ListShape, NodeShape, ScalarShape}
 import ml.combust.mleap.runtime.function.UserDefinedFunction
 import ml.combust.mleap.runtime.frame.{SimpleTransformer, Transformer}
 
@@ -11,5 +11,8 @@ import ml.combust.mleap.runtime.frame.{SimpleTransformer, Transformer}
 case class ReverseStringIndexer(override val uid: String = Transformer.uniqueName("reverse_string_indexer"),
                                 override val shape: NodeShape,
                                 override val model: ReverseStringIndexerModel) extends SimpleTransformer {
-  override val exec: UserDefinedFunction = (value: Double) => model(value.toInt)
+  override val exec: UserDefinedFunction = model.inputShape match {
+    case ScalarShape(false) => (value: Double) => model(value.toInt)
+    case ListShape(false) => (value: Seq[Double]) => model(value.map(_.toInt))
+  }
 }

7dce151cd690dccf76ed5d0e92182edbc2871480
Hollin Wilkins <hollinrwilkins@gmail.com>
2018-03-28 08:27:12 -0700

    Improve reverse string indexer with list inputs/outputs. (#350)

--- mleap-core/src/test/scala/ml/combust/mleap/core/feature/ReverseStringIndexerModelSpec.scala
+++ mleap-core/src/test/scala/ml/combust/mleap/core/feature/ReverseStringIndexerModelSpec.scala
@@ -1,12 +1,12 @@
 package ml.combust.mleap.core.feature
 
-import ml.combust.mleap.core.types.{ScalarType, StructField}
+import ml.combust.mleap.core.types._
 import org.scalatest.FunSpec
 
 class ReverseStringIndexerModelSpec extends FunSpec {
 
   describe("reverse string indexer model") {
-    val model = new ReverseStringIndexerModel(Seq("one", "two", "three"))
+    val model = ReverseStringIndexerModel(Seq("one", "two", "three"))
 
     it("has the right input schema") {
       assert(model.inputSchema.fields ==
@@ -16,6 +16,42 @@ class ReverseStringIndexerModelSpec extends FunSpec {
     it("has the right output schema") {
       assert(model.outputSchema.fields ==
         Seq(StructField("output", ScalarType.String)))
+    }
+  }
+
+  describe("with a list input shape") {
+    val model = ReverseStringIndexerModel(Seq("one", "two", "three"), ListShape(false))
+
+    it("has the right input schema") {
+      assert(model.inputSchema.fields ==
+        Seq(StructField("input", ListType.Double.nonNullable)))
+    }
+
+    it("has the right output schema") {
+      assert(model.outputSchema.fields ==
+        Seq(StructField("output", ListType.String)))
+    }
+  }
+
+  describe("invalid shapes") {
+    it("tensor shape throws an IllegalArgumentException") {
+      assertThrows[IllegalArgumentException] {
+        ReverseStringIndexerModel(Seq("one", "two", "three"), TensorShape(None, isNullable = false))
+      }
+    }
+
+    it("nullable shape throws an IllegalArgumentException") {
+      assertThrows[IllegalArgumentException] {
+        ReverseStringIndexerModel(Seq("one", "two", "three"), ScalarShape(true))
+      }
+
+      assertThrows[IllegalArgumentException] {
+        ReverseStringIndexerModel(Seq("one", "two", "three"), TensorShape(None, true))
+      }
+
+      assertThrows[IllegalArgumentException] {
+        ReverseStringIndexerModel(Seq("one", "two", "three"), ListShape(true))
+      }
     }
   }
 }
--- mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/ReverseStringIndexerSpec.scala
+++ mleap-runtime/src/test/scala/ml/combust/mleap/runtime/transformer/feature/ReverseStringIndexerSpec.scala
@@ -2,18 +2,43 @@ package ml.combust.mleap.runtime.transformer.feature
 
 import ml.combust.mleap.core.feature.ReverseStringIndexerModel
 import ml.combust.mleap.core.types._
+import ml.combust.mleap.runtime.frame.{DefaultLeapFrame, Row}
 import org.scalatest.FunSpec
 
 class ReverseStringIndexerSpec extends FunSpec {
+  val scalarFrame = DefaultLeapFrame(
+    StructType(StructField("input", ScalarType.Double.nonNullable)).get,
+    Seq(Row(1.0))
+  )
 
-  describe("input/output schema") {
+  val listFrame = DefaultLeapFrame(
+    StructType(StructField("input", ListType.Double.nonNullable)).get,
+    Seq(Row(Seq(1.0, 2.0)))
+  )
+
+  describe("scalar input/output") {
     it("has the correct inputs and outputs") {
       val transformer = ReverseStringIndexer(shape = NodeShape.feature(),
-        model = new ReverseStringIndexerModel(Seq("one", "two", "three")))
+        model = ReverseStringIndexerModel(Seq("one", "two", "three")))
 
       assert(transformer.schema.fields ==
         Seq(StructField("input", ScalarType.Double.nonNullable),
           StructField("output", ScalarType.String)))
+
+      assert(transformer.transform(scalarFrame).get.dataset.head.getString(1) == "two")
+    }
+  }
+
+  describe("list input/output") {
+    it("has the correct inputs and outputs") {
+      val transformer = ReverseStringIndexer(shape = NodeShape.feature(),
+        model = ReverseStringIndexerModel(Seq("one", "two", "three"), ListShape(false)))
+
+      assert(transformer.schema.fields ==
+        Seq(StructField("input", ListType.Double.nonNullable),
+          StructField("output", ListType.String)))
+
+      assert(transformer.transform(listFrame).get.dataset.head.getSeq[String](1) == Seq("two", "three"))
     }
   }
 }

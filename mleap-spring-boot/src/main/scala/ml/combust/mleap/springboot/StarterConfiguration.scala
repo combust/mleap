a0a30f120bb924553eef5ba222d5a94346810adf
Anca Sarb <sarb.anca@gmail.com>
2018-04-12 13:22:37 +0100

    make protos work with Spring and ProtobufHttpMessageConverter

--- mleap-spring-boot/src/main/scala/ml/combust/mleap/springboot/StarterConfiguration.scala
+++ mleap-spring-boot/src/main/scala/ml/combust/mleap/springboot/StarterConfiguration.scala
@@ -3,18 +3,24 @@ package ml.combust.mleap.springboot
 import akka.actor.ActorSystem
 import ml.combust.mleap.executor.MleapExecutor
 import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter
 
 @Configuration
 class StarterConfiguration {
 
   @Bean
-  def system() = {
+  def actorSystem() = {
     ActorSystem("MleapSpringBootScoring")
   }
 
   @Bean
-  def mleapExecutor(system: ActorSystem) = {
-    MleapExecutor(system)
+  def mleapExecutor(actorSystem: ActorSystem) = {
+    MleapExecutor(actorSystem)
+  }
+
+  @Bean
+  def protobufHttpMessageConverter() = {
+    new ProtobufHttpMessageConverter
   }
 
 }
--- project/Protobuf.scala
+++ project/Protobuf.scala
@@ -5,7 +5,9 @@ import sbt._
 import sbtprotoc.ProtocPlugin.autoImport.PB
 
 object Protobuf {
-  lazy val bundleSettings = Seq(PB.targets in Compile := Seq(scalapb.gen(flatPackage = true) -> (sourceManaged in Compile).value),
+  lazy val bundleSettings = Seq(PB.targets in Compile := Seq(
+    PB.gens.java -> (sourceManaged in Compile).value,
+    scalapb.gen(flatPackage = true, javaConversions = true) -> (sourceManaged in Compile).value),
     PB.includePaths in Compile := Seq(file("bundle-protobuf/bundle")),
     PB.protoSources in Compile := Seq(file("bundle-protobuf/bundle")))
 
@@ -13,7 +15,9 @@ object Protobuf {
     PB.includePaths in Compile := Seq(file("bundle-protobuf/mleap"), file("bundle-protobuf/grpc")),
     PB.protoSources in Compile := Seq(file("bundle-protobuf/mleap"), file("bundle-protobuf/grpc")))
 
-  lazy val springBootSettings = Seq(PB.targets in Compile := Seq(scalapb.gen(flatPackage = true) -> (sourceManaged in Compile).value),
+  lazy val springBootSettings = Seq(PB.targets in Compile := Seq(
+    PB.gens.java -> (sourceManaged in Compile).value,
+    scalapb.gen(flatPackage = true, javaConversions = true) -> (sourceManaged in Compile).value),
     PB.includePaths in Compile := Seq(file("bundle-protobuf/mleap")),
     PB.protoSources in Compile := Seq(file("bundle-protobuf/mleap")))
 }

f9aa623af013dc3c45a66f38dfada579131887cf
Noah Pritikin <noah.pritikin@kount.com>
2018-05-20 21:29:44 -0600

    Interpolate environments variables in typesafe config.

--- mleap-serving/src/main/scala/ml/combust/mleap/serving/MleapConfig.scala
+++ mleap-serving/src/main/scala/ml/combust/mleap/serving/MleapConfig.scala
@@ -1,14 +1,13 @@
 package ml.combust.mleap.serving
 
 import com.typesafe.config.Config
-import scala.util.Properties
 
 /**
   * Created by hollinwilkins on 1/31/17.
   */
 case class HttpConfig(config: Config) {
-  val bindHostname = scala.util.Properties.envOrElse("MLEAP_SERVER_HOSTNAME", config.getString("bind-hostname"))
-  val bindPort = scala.util.Properties.envOrElse("MLEAP_SERVER_PORT", config.getString("bind-port")).toInt
+  val bindHostname = config.getString("bind-hostname")
+  val bindPort = config.getInt("bind-port")
 }
 
 case class MleapConfig(config: Config) {

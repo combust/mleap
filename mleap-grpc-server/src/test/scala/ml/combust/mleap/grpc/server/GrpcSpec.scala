8c6b1387d52c2ef3f3a981e3669190cb8ece7834
Hollin Wilkins <hollinrwilkins@gmail.com>
2018-06-06 16:37:42 -0700

    Resource clean up tests for gRPC.

--- mleap-grpc-server/src/test/scala/ml/combust/mleap/grpc/server/GrpcSpec.scala
+++ mleap-grpc-server/src/test/scala/ml/combust/mleap/grpc/server/GrpcSpec.scala
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, KillSwitches}
-import akka.stream.scaladsl.{Keep, Source}
+import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.stream.testkit.scaladsl.TestSink
 import akka.testkit.TestKit
 import io.grpc.{ManagedChannel, Server, Status, StatusRuntimeException}
@@ -17,6 +17,7 @@ import org.scalatest.concurrent.ScalaFutures
 
 import scala.concurrent.duration._
 import ml.combust.mleap.grpc.server.TestUtil._
+import org.scalatest.time.Span
 
 import scala.concurrent.Await
 import scala.util.Try
@@ -68,11 +69,9 @@ class GrpcSpec extends TestKit(ActorSystem("grpc-server-test"))
 
     it("transforms a row using row flow") {
       val uuid = UUID.randomUUID()
-      val ((switch, done), stream) = Source.fromIterator(() => frame.get.dataset.iterator.map(row => (Try(row), uuid)))
+      val stream = Source.fromIterator(() => frame.get.dataset.iterator.map(row => (Try(row), uuid)))
         .via(client.rowFlow(lrUri, StreamRowSpec(frame.get.schema)))
-        .viaMat(KillSwitches.single)(Keep.right)
-        .watchTermination()(Keep.both)
-        .toMat(TestSink.probe(system))(Keep.both)
+        .toMat(TestSink.probe(system))(Keep.right)
         .run()(ActorMaterializer.create(system))
 
       val result = stream
@@ -82,9 +81,18 @@ class GrpcSpec extends TestKit(ActorSystem("grpc-server-test"))
       assert(result._1.isSuccess)
       val row = result._1.get.get
       assert(row.getDouble(26) == 232.62463916840318)
+    }
 
-      switch.shutdown()
-      Await.result(done, 10.seconds)
+    it("shuts down the row stream when upstream completes") {
+      val uuid = UUID.randomUUID()
+
+      val done = Source.single((Try(frame.get.dataset.head), uuid))
+        .via(client.rowFlow(lrUri, StreamRowSpec(frame.get.schema)))
+        .watchTermination()(Keep.right)
+        .to(Sink.ignore)
+        .run()(ActorMaterializer.create(system))
+
+      done.isReadyWithin(2.seconds)
     }
 
     it("returns error with the right exception when using row flow"){
@@ -112,6 +120,17 @@ class GrpcSpec extends TestKit(ActorSystem("grpc-server-test"))
       assert(result._1.isSuccess)
       val row = result._1.get.dataset.toArray.apply(0)
       assert(row.getDouble(26) == 232.62463916840318)
+    }
+
+    it("shuts down the frame flow when upstream completes") {
+      val uuid = UUID.randomUUID()
+      val done = Source.single((TransformFrameRequest(frame), uuid))
+        .via(client.frameFlow(uri = lrUri))
+        .watchTermination()(Keep.right)
+        .to(Sink.ignore)
+        .run()(ActorMaterializer.create(system))
+      
+      done.isReadyWithin(2.seconds)
     }
   }
 }

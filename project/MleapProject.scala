9fd7e919433038a53f9dd57bd36d34242c28bf23
Hollin Wilkins <hollinrwilkins@gmail.com>
2018-09-27 18:29:02 -0700

    Get rid of XGBoost integration for Spark 2.4 for now.

--- project/MleapProject.scala
+++ project/MleapProject.scala
@@ -141,12 +141,10 @@ object MleapProject {
       bundleMl,
       spark,
       sparkExtension,
-      tensorflow,
-      xgboostSpark)
+      tensorflow)
   ).settings(excludeDependencies ++= Seq(
     SbtExclusionRule("org.tensorflow"),
-    SbtExclusionRule("org.apache.spark"),
-    SbtExclusionRule("ml.dmlc")
+    SbtExclusionRule("org.apache.spark")
   ))
 
   lazy val databricksRuntime = Project(

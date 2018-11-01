267df12fd6b6af5fc558d1bfa317bebadf87cc62
Hollin Wilkins <hollinwilkins@Hollins-MacBook-Pro-2.local>
2018-10-31 22:33:22 -0700

    Revert "Get rid of XGBoost integration for Spark 2.4 for now."
    This reverts commit 9fd7e919433038a53f9dd57bd36d34242c28bf23.

--- project/MleapProject.scala
+++ project/MleapProject.scala
@@ -141,10 +141,12 @@ object MleapProject {
       bundleMl,
       spark,
       sparkExtension,
-      tensorflow)
+      tensorflow,
+      xgboostSpark)
   ).settings(excludeDependencies ++= Seq(
     SbtExclusionRule("org.tensorflow"),
-    SbtExclusionRule("org.apache.spark")
+    SbtExclusionRule("org.apache.spark"),
+    SbtExclusionRule("ml.dmlc")
   ))
 
   lazy val databricksRuntime = Project(

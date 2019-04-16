7d2078be6e0f6156c0a56bbfbf71b0e9a4f8ac35
Anca Sarb <sarb.anca@gmail.com>
2019-04-17 00:45:44 +0100

    fix java.io.NotSerializableException in tf model (#515)

--- mleap-tensorflow/src/test/scala/ml/combust/mleap/tensorflow/TensorflowModelSpec.scala
+++ mleap-tensorflow/src/test/scala/ml/combust/mleap/tensorflow/TensorflowModelSpec.scala
@@ -8,11 +8,15 @@ import org.scalatest.FunSpec
   * Created by hollinwilkins on 1/12/17.
   */
 class TensorflowModelSpec extends FunSpec {
+
   describe("with an adding tensorflow model") {
     it("adds two floats together") {
-      val model = TensorflowModel(TestUtil.createAddGraph(),
+      val graph = TestUtil.createAddGraph()
+      val model = TensorflowModel(graph = Some(graph),
         inputs = Seq(("InputA", TensorType.Float()), ("InputB", TensorType.Float())),
-        outputs = Seq(("MyResult", TensorType.Float())))
+        outputs = Seq(("MyResult", TensorType.Float())),
+        graphBytes = graph.toGraphDef
+      )
 
       assert(model(Tensor.scalar(23.4f), Tensor.scalar(45.6f)).head == Tensor.scalar(23.4f + 45.6f))
       assert(model(Tensor.scalar(42.3f), Tensor.scalar(99.9f)).head == Tensor.scalar(42.3f + 99.9f))
@@ -25,13 +29,17 @@ class TensorflowModelSpec extends FunSpec {
   describe("with a multiple tensorflow model") {
     describe("with a float and a float vector") {
       it("scales the float vector") {
-        val model = TensorflowModel(TestUtil.createMultiplyGraph(),
+        val graph = TestUtil.createMultiplyGraph()
+        val model = TensorflowModel(graph = Some(graph),
           inputs = Seq(("InputA", TensorType.Float()), ("InputB", TensorType.Float())),
-          outputs = Seq(("MyResult", TensorType.Float(3))))
+          outputs = Seq(("MyResult", TensorType.Float(3))),
+          graphBytes = graph.toGraphDef)
         val tensor1 = DenseTensor(Array(1.0f, 2.0f, 3.0f), Seq(3))
         val scale1 = Tensor.scalar(2.0f)
 
         assert(model(scale1, tensor1).head.asInstanceOf[DenseTensor[Float]].values sameElements Array(2.0f, 4.0f, 6.0f))
+
+        model.close()
       }
     }
   }

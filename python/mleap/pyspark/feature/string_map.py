cce1b59713f90c4abd6512cddd8ece4868882a0f
juho-autio <juho.autio@rovio.com>
2019-09-24 00:22:51 +0300

    Renamed feature.py -> string_map.py, under new package "feature"

--- python/mleap/pyspark/feature/string_map.py
+++ python/mleap/pyspark/feature/string_map.py
@@ -19,7 +19,7 @@ class StringMap(JavaTransformer, HasInputCol, HasOutputCol, JavaMLReadable, Java
             .mapAsScalaMapConverter(labels) \
             .asScala() \
             .toMap(_jvm().scala.Predef.conforms())
-        handle_invalid_jvm = _jvm().ml.combust.mleap.core.feature.HandleInvalid.__getattr__(
+        handle_invalid_jvm = _jvm().ml.combust.mleap.core.feature.StringMapHandleInvalid.__getattr__(
             handleInvalid.capitalize() + '$').__getattr__('MODULE$')
         string_map_model = self._new_java_obj("ml.combust.mleap.core.feature.StringMapModel",
                                               labels_scala_map, handle_invalid_jvm, defaultValue)
@@ -28,11 +28,12 @@ class StringMap(JavaTransformer, HasInputCol, HasOutputCol, JavaMLReadable, Java
         self.setOutputCol(outputCol)
 
     @classmethod
-    def from_dataframe(cls, df, key_col, value_col):
+    def from_dataframe(cls, df, key_col, value_col, handleInvalid='error', defaultValue=0.0):
         """
         from_dataframe(self, df, key_col, value_col)
         Creates StringMap from a DataFrame.
         """
         dict_from_df = {row[0]: float(row[1]) for row in
                         df.select([key_col, value_col]).collect()}
-        return cls(dict_from_df, key_col, value_col)
+        return cls(dict_from_df, inputCol=key_col, outputCol=value_col, handleInvalid=handleInvalid,
+                   defaultValue=defaultValue)

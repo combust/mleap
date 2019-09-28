d60518e6554120c8c08237a60b6de2ee5a9ff1c4
juho-autio <juho.autio@rovio.com>
2019-09-28 10:30:56 +0300

    Revert "Avoid overhead of invoking sbt twice in travis.sh"
    That didn't work. Why? Did sbt clean the target or something?

--- travis/travis.sh
+++ travis/travis.sh
@@ -3,7 +3,9 @@
 if [[ $TRAVIS_BRANCH == 'master' ]] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   source travis/extract.sh
   source travis/docker.sh
-  sbt "+ mleap-spark-extension/writeRuntimeClasspathToFile" "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test" "+ publishSigned" "mleap-xgboost-runtime/publishSigned" "mleap-xgboost-spark/publishSigned" "mleap-serving/docker:publish" "mleap-spring-boot/docker:publish"
+  sbt "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test" "+ publishSigned" "mleap-xgboost-runtime/publishSigned" "mleap-xgboost-spark/publishSigned" "mleap-serving/docker:publish" "mleap-spring-boot/docker:publish"
 else
-  sbt "+ mleap-spark-extension/writeRuntimeClasspathToFile" "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test"
+  sbt "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test"
 fi
+# pyspark tests require this, so run this separately in case 'sbt test' above gives up due to failures
+sbt "mleap-spark-extension/writeRuntimeClasspathToFile"

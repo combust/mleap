4d033e048d1123eed48cd58955f1e3811ec389a9
juho-autio <juho.autio@rovio.com>
2019-09-28 09:57:31 +0300

    Avoid overhead of invoking sbt twice in travis.sh

--- travis/travis.sh
+++ travis/travis.sh
@@ -3,9 +3,7 @@
 if [[ $TRAVIS_BRANCH == 'master' ]] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   source travis/extract.sh
   source travis/docker.sh
-  sbt "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test" "+ publishSigned" "mleap-xgboost-runtime/publishSigned" "mleap-xgboost-spark/publishSigned" "mleap-serving/docker:publish" "mleap-spring-boot/docker:publish"
+  sbt "+ mleap-spark-extension/writeRuntimeClasspathToFile" "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test" "+ publishSigned" "mleap-xgboost-runtime/publishSigned" "mleap-xgboost-spark/publishSigned" "mleap-serving/docker:publish" "mleap-spring-boot/docker:publish"
 else
-  sbt "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test"
+  sbt "+ mleap-spark-extension/writeRuntimeClasspathToFile" "+ test" "+ mleap-executor-tests/test" "+ mleap-benchmark/test" "mleap-xgboost-runtime/test" "mleap-xgboost-spark/test"
 fi
-# pyspark tests require this, so run this separately in case 'sbt test' above gives up due to failures
-sbt "mleap-spark-extension/writeRuntimeClasspathToFile"

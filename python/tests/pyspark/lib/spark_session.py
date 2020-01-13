c44131a6719fe78004a5a81b4934dbbb313933b2
juhoautio <autiojuho@gmail.com>
2020-01-13 20:15:15 +0200

    Mention `sbt +compile` in spark_session to build with all versions

--- python/tests/pyspark/lib/spark_session.py
+++ python/tests/pyspark/lib/spark_session.py
@@ -40,10 +40,10 @@ def _mleap_classpath():
     as all jars are with the same scala version.
 
     Classpath file can be refreshed manually by running `sbt mleap-spark-extension/writeRuntimeClasspathToFile`.
-    However, that's only needed if making changes to dependencies, and any sbt compile runs writeRuntimeClasspathToFile
+    However, that's only needed if making changes to dependencies, and any sbt +compile runs writeRuntimeClasspathToFile
     """
     classpath_file = os.path.join(os.path.dirname(__file__), '..', '..', '..', '..',
                                   'mleap-spark-extension', 'target', 'classpath-runtime_2.11.8.txt')
-    assert os.path.exists(classpath_file), 'classpath-runtime.txt is missing. Run sbt compile first to generate it.'
+    assert os.path.exists(classpath_file), 'classpath-runtime.txt is missing. Run sbt +compile first to generate it.'
     with open(classpath_file, 'r') as f:
         return f.read()

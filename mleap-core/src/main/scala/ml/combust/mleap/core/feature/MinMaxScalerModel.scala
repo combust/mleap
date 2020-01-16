ffe5715d1af430815cd0ee28f6d07137f5d164aa
asarb <asarb@expedia.com>
2020-01-16 16:35:07 +0000

    fix different scores for MinMaxScalerModel in spark vs mleap

--- mleap-core/src/main/scala/ml/combust/mleap/core/feature/MinMaxScalerModel.scala
+++ mleap-core/src/main/scala/ml/combust/mleap/core/feature/MinMaxScalerModel.scala
@@ -15,50 +15,35 @@ import scala.math.{max, min}
   * @param originalMin minimum values from training features
   * @param originalMax maximum values from training features
   */
-@SparkCode(uri = "https://github.com/apache/spark/blob/v2.0.0/mllib/src/main/scala/org/apache/spark/ml/feature/MinMaxScaler.scala")
+@SparkCode(uri = "https://github.com/apache/spark/blob/v2.4.0/mllib/src/main/scala/org/apache/spark/ml/feature/MinMaxScaler.scala")
 case class MinMaxScalerModel(originalMin: Vector,
                              originalMax: Vector) extends Model {
   val originalRange = (originalMax.toBreeze - originalMin.toBreeze).toArray
   val minArray = originalMin.toArray
 
+  val minValue = 0.0
+  val maxValue = 1.0
+
   /**Scale a feature vector using the min/max
     *
     * @param vector feature vector
-    * @return scaled feature fector
+    * @return scaled feature vector
     */
   def apply(vector: Vector): Vector = {
-    vector match {
-      case DenseVector(values) =>
-        val vs = values.clone()
-        val size = vs.length
-        var i = 0
-        while (i < size) {
-          if (!values(i).isNaN) {
-            val raw = if (originalRange(i) != 0) {
-              max(min(1.0, (values(i) - minArray(i)) / originalRange(i)), 0.0)
-            } else {
-              0.5
-            }
-            vs(i) = raw
-          }
-          i += 1
-        }
-        Vectors.dense(vs)
-      case SparseVector(size, indices, values) =>
-        val vs = values.clone()
-        val nnz = vs.length
-        var i = 0
-        while (i < nnz) {
-          val raw = if (originalRange(i) != 0) {
-            max(min(1.0, (indices(i) - minArray(i)) / originalRange(i)), 0.0)
-          } else {
-            0.5
-          }
-          vs(i) = raw
-          i += 1
-        }
-        Vectors.sparse(size, indices, vs)
+    val scale = maxValue - minValue
+
+    // 0 in sparse vector will probably be rescaled to non-zero
+    val values = vector.copy.toArray
+    val size = values.length
+    var i = 0
+    while (i < size) {
+      if (!values(i).isNaN) {
+        val raw = if (originalRange(i) != 0) (values(i) - minArray(i)) / originalRange(i) else 0.5
+        values(i) = raw * scale + minValue
+      }
+      i += 1
     }
+    Vectors.dense(values)
   }
 
   override def inputSchema: StructType = StructType("input" -> TensorType.Double(originalRange.length)).get

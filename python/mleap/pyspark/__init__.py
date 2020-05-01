40dcc5d9b045864589fed4e3f76396b328f48416
asarb <asarb@expedia.com>
2020-05-01 19:20:22 +0100

    test PR

--- python/mleap/pyspark/__init__.py
+++ python/mleap/pyspark/__init__.py
@@ -4,14 +4,7 @@ import pyspark.ml
 
 import mleap.pyspark
 import mleap.pyspark.feature
-from mleap.pyspark.feature.string_map import StringMap
-from mleap.pyspark.feature.math_unary import MathUnary
 
 sys.modules['pyspark.ml.mleap'] = mleap
 sys.modules['pyspark.ml.mleap.pyspark'] = sys.modules['mleap.pyspark']
 sys.modules['pyspark.ml.mleap.feature'] = sys.modules['mleap.pyspark.feature']
-
-sys.modules['pyspark.ml'].mleap = mleap
-sys.modules['pyspark.ml'].mleap.feature = sys.modules['mleap.pyspark.feature']
-sys.modules['pyspark.ml'].mleap.feature.StringMap = StringMap
-sys.modules['pyspark.ml'].mleap.feature.MathUnary = MathUnary

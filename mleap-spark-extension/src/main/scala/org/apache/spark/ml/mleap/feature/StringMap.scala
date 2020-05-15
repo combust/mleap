0eeb699630d095cf1a5f799a382d617075a6fa1e
wenjuan <wenjuan@tubi.tv>
2020-05-15 14:09:32 +0800

    add MLReader&MLWriter

--- mleap-spark-extension/src/main/scala/org/apache/spark/ml/mleap/feature/StringMap.scala
+++ mleap-spark-extension/src/main/scala/org/apache/spark/ml/mleap/feature/StringMap.scala
@@ -1,15 +1,12 @@
 package org.apache.spark.ml.mleap.feature
 
-import ml.combust.mleap.core.feature.StringMapModel
+import ml.combust.mleap.core.feature.{StringMapHandleInvalid, StringMapModel}
+import org.apache.hadoop.fs.Path
 import org.apache.spark.annotation.DeveloperApi
 import org.apache.spark.ml.Transformer
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.ml.param.shared.{HasInputCol, HasOutputCol}
-import org.apache.spark.ml.util.{
-  DefaultParamsReadable,
-  DefaultParamsWritable,
-  Identifiable
-}
+import org.apache.spark.ml.util._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, Dataset}
 import org.apache.spark.sql.types._
@@ -21,7 +18,7 @@ class StringMap(override val uid: String, val model: StringMapModel)
     extends Transformer
     with HasInputCol
     with HasOutputCol
-    with DefaultParamsWritable {
+    with MLWritable {
   def this(model: StringMapModel) =
     this(uid = Identifiable.randomUID("string_map"), model = model)
 
@@ -54,8 +51,61 @@ class StringMap(override val uid: String, val model: StringMapModel)
 
     StructType(schema.fields :+ StructField($(outputCol), DoubleType))
   }
+
+  override def write: MLWriter = new StringMap.StringMapWriter(this)
+
 }
 
-object StringMap extends DefaultParamsReadable[StringMap] {
+object StringMap extends MLReadable[StringMap] {
+
+  override def read: MLReader[StringMap] = new StringMapReader
+
   override def load(path: String): StringMap = super.load(path)
+
+  private class StringMapWriter(instance: StringMap) extends MLWriter {
+
+    private case class Data(labels: Map[String, Double], handleInvalid: String, defaultValue: Double)
+
+    override protected def saveImpl(path: String): Unit = {
+      // Save metadata and Params
+      DefaultParamsWriter.saveMetadata(instance, path, sc)
+      // Save model data: fromTypeName, toTypeName
+      val model = instance.model
+      val labels = model.labels
+      val handleInvalid = model.handleInvalid.asParamString
+      val defaultValue = model.defaultValue
+
+      val data = Data(labels, handleInvalid, defaultValue)
+      val dataPath = new Path(path, "data").toString
+      sparkSession
+        .createDataFrame(Seq(data))
+        .repartition(1)
+        .write
+        .parquet(dataPath)
+    }
+  }
+
+  private class StringMapReader extends MLReader[StringMap] {
+
+    /** Checked against metadata when loading model */
+    private val className = classOf[StringMap].getName
+
+    override def load(path: String): StringMap = {
+      val metadata = DefaultParamsReader.loadMetadata(path, sc, className)
+
+      val dataPath = new Path(path, "data").toString
+
+      val data = sparkSession.read.parquet(dataPath).select("labels", "handleInvalid", "defaultValue").head()
+      val labels = data.getAs[Map[String, Double]](0)
+      val handleInvalid = data.getAs[StringMapHandleInvalid](1)
+      val defaultValue = data.getAs[Double](2)
+
+      val model = new StringMapModel(labels, handleInvalid = handleInvalid., defaultValue = defaultValue)
+      val StringMap = new StringMap(metadata.uid, model)
+
+      metadata.getAndSetParams(StringMap)
+      StringMap
+    }
+  }
+
 }

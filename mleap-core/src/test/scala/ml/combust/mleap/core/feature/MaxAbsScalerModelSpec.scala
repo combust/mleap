1898cbdbebd536708e05d03c8d8ffbab56331ed5
WeichenXu <weichen.xu@databricks.com>
2020-05-16 08:26:29 +0800

    add Spark test dependencies and change parity tooling to allow numerical tolerance (#680)
    add Spark test dependencies and change parity tooling to allow numerical tolerance.

--- mleap-core/src/test/scala/ml/combust/mleap/core/feature/MaxAbsScalerModelSpec.scala
+++ mleap-core/src/test/scala/ml/combust/mleap/core/feature/MaxAbsScalerModelSpec.scala
@@ -4,6 +4,8 @@ import ml.combust.mleap.core.types.{StructField, TensorType}
 import org.apache.spark.ml.linalg.Vectors
 import org.scalatest.FunSpec
 
+import org.apache.spark.ml.util.TestingUtils._
+
 /**
   * Created by mikhail on 9/18/16.
   */
@@ -12,10 +14,9 @@ class MaxAbsScalerModelSpec extends FunSpec {
     val scaler = MaxAbsScalerModel(Vectors.dense(Array(20.0, 10.0, 10.0, 20.0)))
 
     it("Scales the vector based on absolute max value"){
-      val inputArray = Array(15.0, -5.0, 5.0, 19.0)
-      val expectedVector = Array(0.75, -0.5, 0.5, 0.95)
-
-      assert(scaler(Vectors.dense(inputArray)).toArray.sameElements(expectedVector))
+      val inputVector = Vectors.dense(15.0, -5.0, 5.0, 19.0)
+      val expectedVector = Vectors.dense(0.75, -0.5, 0.5, 0.95)
+      assert(scaler(inputVector) ~= expectedVector relTol 1E-9)
     }
 
     it("Has the right input schema") {

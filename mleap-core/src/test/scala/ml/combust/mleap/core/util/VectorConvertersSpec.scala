d05295b60f4ab972911d62e0ff76122975cc92ac
Colm O'Shaughnessy <69862798+colmosh@users.noreply.github.com>
2021-12-16 21:08:24 +0100

    Fixing conversion for Mleap vectors with non-increasing indices (#794)
    * Fixing conversion for Mleap vector with non-increasing indices
    
    * Adding tests for Mleap to Spark conversions

---
+++ mleap-core/src/test/scala/ml/combust/mleap/core/util/VectorConvertersSpec.scala
@@ -0,0 +1,31 @@
+package ml.combust.mleap.core.util
+
+import ml.combust.mleap.tensor.{DenseTensor, SparseTensor}
+import org.scalatest.FunSpec
+
+class VectorConvertersSpec extends FunSpec {
+  describe("mleapTensorToSparkVector works when") {
+
+    it("using a Sparse Tensor") {
+      val vec = Seq(1.0, 0, 3.0)
+      val indices = Seq(0, 2).map(Seq(_))
+      val values = indices.map((i: Seq[Int]) => vec(i.head)).toArray
+      val tensor = SparseTensor(indices, values, Seq(vec.length))
+      assert(VectorConverters.mleapTensorToSparkVector(tensor).toArray sameElements vec)
+    }
+
+    it("using a Sparse Tensor with non-increasing indices") {
+      val vec = Seq(1.0, 0, 3.0)
+      val indices = Seq(2, 0).map(Seq(_))
+      val values = indices.map((i: Seq[Int]) => vec(i.head)).toArray
+      val tensor = SparseTensor(indices, values, Seq(vec.length))
+      assert(VectorConverters.mleapTensorToSparkVector(tensor).toArray sameElements vec)
+    }
+
+    it("using a Dense Tensor") {
+      val vec = Seq(1.0, 2.0, 3.0).toArray
+      val tensor = DenseTensor(vec, Seq(vec.length))
+      assert(VectorConverters.mleapTensorToSparkVector(tensor).toArray sameElements vec)
+    }
+  }
+}

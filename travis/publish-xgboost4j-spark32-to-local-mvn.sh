ce7f9fe71e3df2521f348a1f76c8c308735aad6b
Weichen Xu <weichen.xu@databricks.com>
2022-01-19 21:44:00 +0800

    fix

--- travis/publish-xgboost4j-spark32-to-local-mvn.sh
+++ travis/publish-xgboost4j-spark32-to-local-mvn.sh
@@ -4,8 +4,8 @@ set -e
 
 git clone https://github.com/WeichenXu123/xgboost.git --depth=1
 cd xgboost
-git fetch origin v151-dbg:v151-dbg
-git checkout v151-dbg
+git fetch origin v151-dbg2:v151-dbg2
+git checkout v151-dbg2
 
 cd jvm-packages/xgboost4j-spark
 

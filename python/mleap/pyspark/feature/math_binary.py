c4751e52f86cc2514dfce8663de766eb18ebfc59
arthurarj <arthurcruz.arj@gmail.com>
2022-09-16 11:51:48 -0400

    Address MathBinary issue with zeros defaults

--- python/mleap/pyspark/feature/math_binary.py
+++ python/mleap/pyspark/feature/math_binary.py
@@ -96,8 +96,8 @@ class MathBinary(JavaTransformer, HasOutputCol, JavaMLReadable, JavaMLWritable):
 
             scalaMathBinaryModel = _jvm().ml.combust.mleap.core.feature.MathBinaryModel(
                 scalaBinaryOperation,
-                Some(defaultA) if defaultA else ScalaNone(),
-                Some(defaultB) if defaultB else ScalaNone(),
+                Some(defaultA) if defaultA is not None else ScalaNone(),
+                Some(defaultB) if defaultB is not None else ScalaNone(),
             )
             self._java_obj = self._new_java_obj(
                 "org.apache.spark.ml.mleap.feature.MathBinary",

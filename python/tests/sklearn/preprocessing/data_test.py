4e5f800a3f350014dbd80059b21c89190c0bd481
jsleight <64800239+jsleight@users.noreply.github.com>
2023-05-09 11:25:51 -0400

    Upgrade tensorflow-java to 0.5.0 which corresponds to tensorflow 2.10.1. (#851)
    Upgrade tensorflow-java to 0.5.0 which corresponds to tensorflow 2.10.1.

--- python/tests/sklearn/preprocessing/data_test.py
+++ python/tests/sklearn/preprocessing/data_test.py
@@ -24,7 +24,7 @@ import numpy as np
 import pandas as pd
 from mleap.sklearn.preprocessing.data import FeatureExtractor, MathUnary, MathBinary, StringMap
 from mleap.sklearn.preprocessing.data import StandardScaler, MinMaxScaler, LabelEncoder, Binarizer, PolynomialFeatures
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 
 
 class TransformerTests(unittest.TestCase):
